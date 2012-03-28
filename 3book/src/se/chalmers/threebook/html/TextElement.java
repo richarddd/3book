e35bf23a6c9a94c1f4322042cfb3fed41c92b5e5
Richard Davison <richard.davison1@gmail.com>
2012-03-28 16:59:53 +0200

    began working on better viewflipper implementation, forgot to add

---
+++ 3book/src/se/chalmers/threebook/html/TextElement.java
@@ -0,0 +1,25 @@
+package se.chalmers.threebook.html;
+
+public class TextElement extends RenderElement {
+	
+	private String text;
+	private StyleFlag style;
+	
+	public TextElement(String text, StyleFlag style) {
+		super();
+		this.text = text;
+		this.style = style;
+	}
+
+	public String getText() {
+		return text;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public StyleFlag getStyle() {
+		return style;
+	}
+}
